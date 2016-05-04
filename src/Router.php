13b15265d81d0b3939c386bba2dd0719168c24b5
Jean-Baptiste Alleaume <jbanety@gmail.com>
2016-05-04 13:49:08 +0200

    avance

---
+++ src/Router.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Part of the ETD Framework Router Package
+ *
+ * @copyright   Copyright (C) 2016 ETD Solutions, SARL Etudoo. Tous droits réservés.
+ * @license     Apache License 2.0; see LICENSE
+ * @author      ETD Solutions http://etd-solutions.com
+ */
+
+namespace EtdSolutions\Router;
+
+use \Joomla\Router\Router as JoomlaRouter;
+
+class Router extends JoomlaRouter {
+
+    
+
+}
